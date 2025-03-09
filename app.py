5b31228ab08f19879ad2215ee462f86d150bbf21
EC2 Default User <ec2-user@ip-172-31-87-81.ec2.internal>
2025-03-09 15:49:54 +0000

    version2: Updated Flask to v2.0 with green color in code

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    color = os.getenv('APP_COLOR', 'blue')
-    return f'<h1 style="color:{color}">Hello, World!</h1>'
+    color = os.getenv('APP_COLOR', 'green')
+    return f'<h1 style="color:{color}">Hello, World! Arpit GUpta 169572237</h1>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
