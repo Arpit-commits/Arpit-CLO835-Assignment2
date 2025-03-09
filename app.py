a8f64424d59f27e5236dec83bfaed7fd205b6874
EC2 Default User <ec2-user@ip-172-31-87-81.ec2.internal>
2025-03-09 14:03:03 +0000

    version1: Initial setup with blue color

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import os
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    color = os.getenv('APP_COLOR', 'blue')
+    return f'<h1 style="color:{color}">Hello, World!</h1>'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
